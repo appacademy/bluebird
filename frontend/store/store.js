93a841c1ff3fa4db6a085ef69898e5e0fc650484
Isak Mladenoff <imladenoff@gmail.com>
2017-11-14 14:14:40 -0800

    Add logger and Redux Dev Tools to middlewares

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
+
 import rootReducer from '../reducers/root';
 import thunk from '../thunk/thunk';
 
-export default (preloadedState = {}) => createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+export default (preloadedState = {}) => createStore(
+  rootReducer, preloadedState,
+  composeWithDevTools(applyMiddleware(createLogger(), thunk))
+);
