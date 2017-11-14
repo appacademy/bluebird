18df6b44adbd4db631db3f25c36977b74d8f1149
Isak Mladenoff <imladenoff@gmail.com>
2017-11-14 14:14:10 -0800

    Fix routes to correctly produce json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, default: {type: :json} do
+  namespace :api, defaults: { format: :json } do
     resources :users do
       resources :chirps, only: [:index]
     end
