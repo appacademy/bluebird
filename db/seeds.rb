44db8faf3d5720494f95a06761da6e1f39cf3e3c
niartenyaw <amwayne90@gmail.com>
2017-11-15 10:27:02 -0800

    Add passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@ User.destroy_all
 Chirp.destroy_all
 Like.destroy_all
 
-user1 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user2 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user3 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user4 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user5 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user6 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user7 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user8 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
-user9 = User.create(username: Faker::Name.name, email: Faker::Internet.email)
+user1 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user2 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user3 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user4 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user5 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user6 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user7 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user8 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
+user9 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'password')
 
 chirp1 = Chirp.create(body: Faker::RickAndMorty.quote, author_id: user1.id)
 chirp2 = Chirp.create(body: Faker::RickAndMorty.quote, author_id: user1.id)
